a8c1ac7cded1b095f702814c58ec66f22fe83981
Kimoto-Norihiro <n000r111code@gmail.com>
2025-02-25 17:17:04 +0900

    add login

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,14 @@
 from app.controller import AnalyzeController, UserController
 from app.database import get_db
-from app.schema import AnalyzeInput, CreateUserInput
+from app.schema import CreateUserInput
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_openai import ChatOpenAI
-from langserve import add_routes
 
 load_dotenv()
 
-db = Depends(get_db)
+db = get_db()
 llm = ChatOpenAI(model="chatgpt-4o-mini", temperature=0)
 
 app = FastAPI()
@@ -23,6 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+db = get_db()
 analyze_controller = AnalyzeController(llm)
 user_controller = UserController(db)
 
@@ -37,9 +37,9 @@ async def get_user_by_firebase_uid(firebase_uid: str):
     return user_controller.get_by_firebase_uid(firebase_uid)
 
 
-add_routes(
-    app, analyze_controller.get_chain(), path="/analyze", input_type=AnalyzeInput
-)
+# add_routes(
+#     app, analyze_controller.get_chain(), path="/analyze", input_type=AnalyzeInput
+# )
 
 if __name__ == "__main__":
     import uvicorn
---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,44 @@
+import { useCreateUserUsersPost } from '@/api/generated';
+import { auth } from '@/lib/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+
+export const useSignUp = () => {
+  const { mutate: createUser } = useCreateUserUsersPost()
+
+  const signUp = async (email: string, password: string) => {
+    try {
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      try {
+        await createUser({
+          data: {
+            firebase_uid: result.user.uid,
+            email: result.user.email!,
+          }
+        });
+      } catch (error) {
+        await result.user.delete();
+        console.error('バックエンドエラー:', error);
+        throw error;
+      }
+    } catch (error) {
+      console.error('Firebase認証エラー:', error);
+      throw error;
+    }
+  };
+
+  return { signUp };
+};
+
+export const useSignIn = () => {
+	const signIn = async (email: string, password: string) => {
+		try {
+			await signInWithEmailAndPassword(auth, email, password);
+		}
+    catch (error) {
+      console.error('ログインエラー:', error);
+      throw error;
+    }
+	}
+
+  return { signIn };
+}
