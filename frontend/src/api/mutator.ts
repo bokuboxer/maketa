a7acec4dd1e2916f8e86cef38cc22d1282e79ec2
Kimoto-Norihiro <n000r111code@gmail.com>
2025-03-02 12:18:25 +0900

    add timeout

--- frontend/src/api/mutator.ts
+++ frontend/src/api/mutator.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
