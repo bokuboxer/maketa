e403fe3c7d0cefcd6fb1ce0495692f7e0c4e1fd1
tetty2525 <121234719+tetty2525@users.noreply.github.com>
2025-03-03 19:28:37 +0900

    fix: use client position

--- frontend/src/app/failures/page.tsx
+++ frontend/src/app/failures/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic';
-
 'use client';
+
+export const dynamic = 'force-dynamic';
 
 import { auth } from '@/lib/firebase';
 import { Loader, Modal } from '@mantine/core';
--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic';
-
 'use client';
+
+export const dynamic = 'force-dynamic';
 
 import { useSignIn } from '@/hooks/useAuth';
 import { auth } from '@/lib/firebase';
