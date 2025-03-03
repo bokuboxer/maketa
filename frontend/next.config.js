e08330aa9320310b064020be5c9334f73fc03962
tetty2525 <121234719+tetty2525@users.noreply.github.com>
2025-03-03 21:03:01 +0900

    fix: add .next to azureApp

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,11 @@ const nextConfig = {
   // Azure Web Apps用の設定を追加
   env: {
     PORT: process.env.PORT || '8080'
-  }
+  },
+  // 追加の設定
+  poweredByHeader: false,
+  generateEtags: true,
+  distDir: '.next'
 }
 
 module.exports = nextConfig 
