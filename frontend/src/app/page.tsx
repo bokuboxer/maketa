3f75a71d4c35fb57b7eef5650579b9fc80b37db0
Kimoto-Norihiro <n000r111code@gmail.com>
2025-03-03 23:41:13 +0900

    fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold text-black">Maketa</h1>
         <p className="text-gray-600">
-          Maketaは失敗を記録し、分析することで成功の糧に変えるサービスです。
+          Maketaは失敗を記録し、分析することで成功の糧に変えるアプリです。
         </p>
       </div>
     </div>
