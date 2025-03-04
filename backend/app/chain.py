868eddc6caabb2a4c97bfd52ea78c9194c3677de
tetty2525 <121234719+tetty2525@users.noreply.github.com>
2025-03-05 01:46:26 +0900

    add: belief_suggest_chain1

--- backend/app/chain.py
+++ backend/app/chain.py
@@ -5,7 +5,7 @@ import app.model as model
 import app.schema as schema
 from app.template import (
     adversity_template,
-    belief_template,
+    belief_suggest_template,
     dispute_template,
     summary_template,
 )
@@ -15,7 +15,13 @@ from langchain_core.language_models import BaseChatModel
 
 template_map = {
     model.ElementType.ADVERSITY: adversity_template,
-    model.ElementType.BELIEF: belief_template,
+    model.ElementType.BELIEF: belief_suggest_template,
+    model.ElementType.DISPUTATION: dispute_template,
+}
+
+analysis_template_map = {
+    model.ElementType.ADVERSITY: adversity_template,
+    model.ElementType.BELIEF: belief_suggest_template,
     model.ElementType.DISPUTATION: dispute_template,
 }
 
@@ -34,20 +40,17 @@ class SuggestChain:
         self.summarize_chain = self.summarize_prompt | self.llm | StrOutputParser()
 
     def run(self, input: schema.SuggestInput) -> schema.AnalysisResult:
-        prompt = PromptTemplate(
-            template=template_map[input.type],
-            input_variables=["text"],
-            partial_variables={
-                "format_instructions": self.json_parser.get_format_instructions()
-            },
-        )
-        chain = prompt | self.llm | self.json_parser
-
-        # Adversityの場合は、直接分析を行う
-        if input.type == model.ElementType.ADVERSITY:
+        if not input.elements:
+            prompt = PromptTemplate(
+                template=template_map[input.type],
+                input_variables=["text"],
+                partial_variables={
+                    "format_instructions": self.json_parser.get_format_instructions()
+                },
+            )
+            chain = prompt | self.llm | self.json_parser
             return chain.invoke({"text": input.text})
 
-        # それ以外の場合は、要素を要約してから分析を行う
         summarized_text = self.summarize_chain.invoke(
             {
                 "analysis_type": input.type.value,
@@ -55,12 +58,18 @@ class SuggestChain:
             }
         )
 
+        prompt = PromptTemplate(
+            template=analysis_template_map[input.type],
+            input_variables=["text"],
+            partial_variables={
+                "format_instructions": self.json_parser.get_format_instructions()
+            },
+        )
+        chain = prompt | self.llm | self.json_parser
         return chain.invoke({"text": summarized_text})
 
     def format_elements(self, elements: List[schema.Element]) -> str:
-        # 要素を番号順にソート
         sorted_elements = sorted(elements, key=lambda x: x.id)
-        # テキスト形式に整形
         return "\n".join(
             [f"{element.id}. {element.description}" for element in sorted_elements]
         )
