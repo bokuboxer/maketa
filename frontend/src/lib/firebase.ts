c4ce857e05a56fdc85a2a9f03edc13804661aa16
Kimoto-Norihiro <n000r111code@gmail.com>
2025-03-04 12:51:00 +0900

    fix

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -3,8 +3,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
