0d3230f90fee3de646a3c0996908d6d13b00a2a8
Kimoto-Norihiro <n000r111code@gmail.com>
2025-03-04 12:41:09 +0900

    fix front and auth

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -3,6 +3,8 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
