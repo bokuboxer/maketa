1fb91e40ea94e4a3e35780f5ad70289684e83d98
Kimoto-Norihiro <n000r111code@gmail.com>
2025-03-04 15:38:50 +0900

    fix

--- backend/server.py
+++ backend/server.py
@@ -50,8 +50,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=600,
 )
 
 db = get_db()
